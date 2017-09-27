2dc5687be0b838d3b70787385f1c833e79df90a8
Hrishikesh <hrishikesh.deodhar@gmail.com>
2017-09-27 15:40:03 +0530

    Simple Kafka Producer and Consumer

---
+++ src/main/java/com/hrishi/kafka/KconsumerApplication.java
@@ -0,0 +1,12 @@
+package com.hrishi.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KconsumerApplication.class, args);
+	}
+}
